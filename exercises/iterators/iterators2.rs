45c93e83050fba52309a27b2c36906b9f85006d7
Ruirui Pan <rrpan@ccnu.edu.cn>
2024-10-08 20:53:49 +0800

    iterators OK

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,8 +6,6 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
@@ -15,7 +13,7 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => first.to_uppercase().to_string() + c.as_str(),
     }
 }
 
@@ -24,7 +22,14 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    // let mut v: Vec<String> = vec![]; // 创建一个存储 `i32` 类型的空向量
+
+    // let mut it = words.iter();
+    // while let Some(word) = it.next() {
+    //     v.push(capitalize_first(&word));
+    // }
+    // v
+    words.iter().map(|&word| capitalize_first(word)).collect()
 }
 
 // Step 3.
@@ -32,7 +37,16 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    // let mut s = String::new();
+    // let mut it = words.iter();
+    // while let Some(word) = it.next() {
+    //     v.push(capitalize_first(&word));
+    // }
+    // v
+
+    // (1..5).iter().map(|x| print!("{x}"));
+
+    words.iter().map(|&word| capitalize_first(word)).collect()
 }
 
 #[cfg(test)]
